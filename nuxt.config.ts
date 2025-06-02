53edd0a873bc0c5535a6a1d3f39895fafc9ec0f9
Yousef Daaboul <yousef.d@fikratech.com>
2025-06-02 11:15:52 +0300

    fixes 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,16 +36,15 @@ export default defineNuxtConfig({
       screenshots: [
         {
           src: "screenshots/desktop_screenshot_1.png",
-          sizes: "1280x720",
-          type: "image/png",
+          sizes: "1387x720",
           form_factor: "wide",
-          label: "Desktop App View",
+          type: "image/png",
         },
         {
           src: "screenshots/mobile_screenshot_1.png",
-          sizes: "720x1280",
+          sizes: "697x1280",
+          form_factor: "narrow",
           type: "image/png",
-          label: "Mobile App View",
         },
       ],
     },
